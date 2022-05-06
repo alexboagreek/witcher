2b2c2fc162d2c26e1f1482de7f67c50b3de9c38f
Alex <olivesargreek@gmail.com>
2022-05-06 11:08:57 +0300

    feat: db, renderTabs

---
+++ js/tabs.js
@@ -0,0 +1,30 @@
+const tabs = option => {
+        const {
+            selectorTabsButton,
+            activeClassButton,
+            selectorTabsElement,
+            activeClassTab,
+            callback = () => {},
+             
+        } = option; 
+
+        const tabsBtns = document.querySelectorAll(selectorTabsButton);
+        const tabsElems = document.querySelectorAll(selectorTabsElement);
+
+        tabsBtns.forEach((btn) => {
+            btn.addEventListener('click', () => {
+                for (let i = 0; i < tabsBtns.length; i++ ) {
+                    if (tabsBtns[i] === btn) {
+                        tabsBtns[i].classList.add(activeClassButton);
+                        tabsElems[i].classList.add(activeClassTab);
+                        callback(i);
+                    } else {
+                        tabsBtns[i].classList.remove(activeClassButton);
+                        tabsElems[i].classList.remove(activeClassTab);
+                    }
+                }
+            });
+        });
+}
+
+export default tabs;
